patrickm68/node-mon-red-project

commit 0eda7ee01301eec888ba71c51c8b0fc75f00d587

HTTP REQUEST: Adding PROPPATCH and PROPFIND http methods (#1531)
* HTTP REQUEST: Adding PROPPATCH and PROPFIND http methods

* Removed method check

nodes/core/io/21-httprequest.js
@@ -135,7 +135,7 @@ module.exports = function(RED) {
             }
             var payload = null;
 
-            if (typeof msg.payload !== "undefined" && (method == "POST" || method == "PUT" || method == "PATCH" ) ) {
+            if (typeof msg.payload !== "undefined") {
                 if (typeof msg.payload === "string" || Buffer.isBuffer(msg.payload)) {
                     payload = msg.payload;
                 } else if (typeof msg.payload == "number") {

commit 213b6f9357ba27c1fd117ca4bc5bf6bd2c64eaa4

Do not include payload in GET requests Fixes #1598
This regression was caused by #1531 - allowing the http request node
use any method.

The full fix is to identify which common verbs must not include a payload
and exclude them. GET needs fixing right now.

nodes/core/io/21-httprequest.js
@@ -135,7 +135,7 @@ module.exports = function(RED) {
             }
             var payload = null;
 
-            if (typeof msg.payload !== "undefined") {
+            if (method !== 'GET' && typeof msg.payload !== "undefined") {
                 if (typeof msg.payload === "string" || Buffer.isBuffer(msg.payload)) {
                     payload = msg.payload;
                 } else if (typeof msg.payload == "number") {